fd8c501e6775e33eadd1200bfa99c6e3354f6397
AdaskoKwi <adamnowakhomm@gmail.com>
2025-01-26 13:26:44 +0100

    Modified application.properties and MessageController.java

--- src/main/java/org/example/gitspring/controller/MessageController.java
+++ src/main/java/org/example/gitspring/controller/MessageController.java
@@ -1,9 +1,11 @@
 package org.example.gitspring.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/message")
 public class MessageController {
 
     @GetMapping("/check")
