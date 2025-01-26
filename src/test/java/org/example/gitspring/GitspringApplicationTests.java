76f5467cdc5417125b380404dbcd633f46c10474
AdaskoKwi <adamnowakhomm@gmail.com>
2025-01-26 12:32:00 +0100

    Initial commit, project created.

---
+++ src/test/java/org/example/gitspring/GitspringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gitspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitspringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
