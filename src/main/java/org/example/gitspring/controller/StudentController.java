b29396dcc9536e47795e6ba613b554c7864568cd
AdaskoKwi <adamnowakhomm@gmail.com>
2025-01-26 14:43:32 +0100

    Added a student class, service and controller. Changed pom.xml and application.properties.

---
+++ src/main/java/org/example/gitspring/controller/StudentController.java
@@ -0,0 +1,41 @@
+package org.example.gitspring.controller;
+
+import org.example.gitspring.model.Student;
+import org.example.gitspring.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/students")
+public class StudentController {
+    StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return studentService.getAllStudents();
+    }
+
+    @GetMapping("/{id}")
+    public Student getStudentById(@PathVariable int id) {
+        return studentService.getStudentById(id);
+    }
+
+    @PostMapping
+    public void addStudent(@RequestBody Student student) {
+        studentService.addStudent(student);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteStudentById(@PathVariable int id) {
+        studentService.deleteStudentById(id);
+    }
+
+}
---
+++ src/main/java/org/example/gitspring/service/StudentService.java
@@ -0,0 +1,34 @@
+package org.example.gitspring.service;
+
+import org.example.gitspring.model.Student;
+import org.example.gitspring.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+    StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public void addStudent(Student student) {
+        studentRepository.save(student);
+    }
+
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    public Student getStudentById(int id) {
+        return studentRepository.findById(id).orElse(null);
+    }
+
+    public void deleteStudentById(int id) {
+        studentRepository.deleteById(id);
+    }
+}
